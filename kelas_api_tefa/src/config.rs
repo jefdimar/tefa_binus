daa1f6cc5b1fcebf6b6a89ea9ba8d4c57c1fe9b4
telkomdev-jefri <jefdimar@gmail.com>
2022-12-19 15:33:30 +0700

    adding kelas tefa

---
+++ kelas_api_tefa/src/config.rs
@@ -0,0 +1,19 @@
+use actix_web::{web, HttpResponse};
+
+// this function could be located in a different module
+pub fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("test") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
+
+// this function could be located in a different module
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| async { HttpResponse::Ok().body("app") }))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)),
+    );
+}
